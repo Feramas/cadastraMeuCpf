c8f6c8dec2bc11f12cb160e0aa6df87fc662308f
Feramas <felipe.ramas@gmail.com>
2020-05-08 17:14:47 -0300

    Primeiro commit

---
+++ src/main/java/cadastraMeuCpf/cadastraMeuCpf/App.java
@@ -0,0 +1,13 @@
+package cadastraMeuCpf.cadastraMeuCpf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
