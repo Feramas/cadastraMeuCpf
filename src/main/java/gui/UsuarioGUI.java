e921b04746d69a716800bb8c6b88e893bc20cec8
Feramas <felipe.ramas@gmail.com>
2020-05-20 09:29:11 -0300

    Adicionando os campos no banco

--- src/main/java/gui/UsuarioGUI.java
+++ src/main/java/gui/UsuarioGUI.java
@@ -7,11 +7,27 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
+import javax.swing.border.TitledBorder;
+
+import dao.UsuarioDAO;
+import modelo.Usuario;
+
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class UsuarioGUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
 
 	/**
 	 * Launch the application.
@@ -43,6 +59,88 @@ public class UsuarioGUI extends JFrame {
 		JLabel lblCadastroDeUsurio = new JLabel("Cadastro de Usuário");
 		lblCadastroDeUsurio.setFont(new Font("Tahoma", Font.BOLD, 18));
 		contentPane.add(lblCadastroDeUsurio, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Cadastrar novo usu\u00E1rio", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		contentPane.add(panel, BorderLayout.CENTER);
+		
+		JLabel lblNome = new JLabel("Nome:");
+		panel.add(lblNome);
+		
+		textField = new JTextField();
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblCpf = new JLabel("CPF:");
+		panel.add(lblCpf);
+		
+		textField_1 = new JTextField();
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblEmail = new JLabel("Email:");
+		panel.add(lblEmail);
+		
+		textField_2 = new JTextField();
+		panel.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblTelefone = new JLabel("Telefone:");
+		panel.add(lblTelefone);
+		
+		textField_3 = new JTextField();
+		panel.add(textField_3);
+		textField_3.setColumns(10);
+		
+		JButton btnCadastrar = new JButton("Cadastrar");
+		btnCadastrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// instanciando a classe Usuario do pacote modelo e criando seu objeto usuarios
+				Usuario usuarios = new Usuario();
+				usuarios.setNome(textField.getText());
+				usuarios.setCpf(textField_1.getText());
+				usuarios.setEmail(textField_2.getText());
+				usuarios.setTelefone(textField_3.getText());
+
+				// fazendo a validação dos dados
+				if ((textField.getText().isEmpty()) || (textField_1.getText().isEmpty()) || (textField_2.getText().isEmpty()) || (textField_3.getText().isEmpty())) {
+				   JOptionPane.showMessageDialog(null, "Os campos não podem retornar vazios");
+				}
+				else {
+
+				    // instanciando a classe UsuarioDAO do pacote dao e criando seu objeto dao
+				    UsuarioDAO dao = new UsuarioDAO();
+				    dao.adiciona(usuarios);
+				    JOptionPane.showMessageDialog(null, "Usuário "+textField.getText()+" inserido com sucesso! ");
+				}
+
+				// apaga os dados preenchidos nos campos de texto
+				textField_1.setText("");
+				textField_2.setText("");
+				textField_3.setText("");
+				textField.setText("");
+			}
+		});
+		panel.add(btnCadastrar);
+		
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textField_1.setText("");
+				textField_2.setText("");
+				textField_3.setText("");
+				textField.setText("");
+			}
+		});
+		panel.add(btnLimpar);
+		
+		JButton btnSair = new JButton("SAIR");
+		btnSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		contentPane.add(btnSair, BorderLayout.SOUTH);
 	}
 
 }
